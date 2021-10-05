fd922f0b6c99de2c3ea753e15affe6b459e6992d
magurman <mattgurman@gmail.com>
2021-10-05 16:50:17 -0400

    initialized spring boot app for client

---
+++ client/src/test/java/com/bsds/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsds.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

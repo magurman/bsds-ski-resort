c5f8b1661965ee6d16f7117ba01385170b839674
waleedhumayon <waleedhumayon@gmail.com>
2021-10-09 19:24:33 -0400

    Individual thread to be controlled by the phase controller

---
+++ client/src/main/java/com/bsds/client/threads/skierThread.java
@@ -0,0 +1,68 @@
+package com.bsds.client.threads;
+
+import com.bsds.client.ApiClient;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+
+
+public class skierThread extends Thread {
+    private final String hostname;
+    private final int port;
+    private final int startSkierID;
+    private int endSkierID;
+    private int startTime;
+    private int endTime;
+    private float nextPhaseTrigger;
+    public CyclicBarrier barrier;
+    private final ApiClient client;
+
+    // Envisioned this as the individual thread that will be responsible for running a certain number of POST requests.
+    // TODO: Find a way to remove the first for loop. Test and make sure it can send requests
+
+    public skierThread(String hostname, int port, int startSkierID, int endSkierID, int startTime,
+                       int endTime, float nextPhaseTrigger, CyclicBarrier barrier) {
+
+        this.hostname = hostname;
+        this.port = port;
+        this.startSkierID = startSkierID;
+        this.endSkierID = endSkierID;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.nextPhaseTrigger = nextPhaseTrigger; // This is important as we have to .await() the cyclic barrier when we reach this point.
+        this.barrier = barrier;
+        this.client = new ApiClient(); // See class ApiClient. Makes the post request
+    }
+
+    @Override
+    public void run() {
+
+        // Empty array of ints, to hold the entire range of skierIDs passed to this thread.
+        ArrayList<Integer> skierIDs = new ArrayList<>();
+        for (int i = startSkierID; i <= endSkierID; i++) {
+            skierIDs.add(i);
+        }
+        // For loop to go through and make a POST request for each skierID in the range.
+        for (int j = startSkierID; j <= endSkierID; j++) {
+            int currentSkierID = getRandomSkierID(skierIDs);
+            skierIDs.remove(currentSkierID);
+            try {
+                this.client.postWriteLifeRide(12, 24, 1, currentSkierID); // Make POST request
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Responsible for getting a random skierID by generating a random index.
+     * @param skierIDs array of all skierIDs
+     * @return a specific skierID, generated randomly
+     */
+    private int getRandomSkierID(ArrayList<Integer> skierIDs) {
+        Random randomGenerator = new Random();
+        int randomIndex = randomGenerator.nextInt(skierIDs.size());
+        return skierIDs.get(randomIndex);
+    }
+}

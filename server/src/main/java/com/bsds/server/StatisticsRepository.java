4c6f23f57775c1cc1a90c4edd8c09d238964c022
vlfranz <vaughnfranz@gmail.com>
2021-11-04 12:27:59 -0400

    further updates to statistics implementation, added tracking to post method as quick first draft can discuss later today

--- server/src/main/java/com/bsds/server/StatisticsRepository.java
+++ server/src/main/java/com/bsds/server/StatisticsRepository.java
@@ -1,9 +1,13 @@
 package com.bsds.server;
 
+import java.util.ArrayList;
+
 import com.bsds.server.db.StatisticsEntity;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface StatisticsRepository extends CrudRepository<StatisticsEntity, Integer>{
+    
+    ArrayList<StatisticsEntity> findByURLAndOperation(String URL, String operation);
     
 }

04c5dbd26692e65af4a0978f686785353b315746
magurman <mattgurman@gmail.com>
2021-12-11 17:16:25 -0500

    added new post endpoint, buildHttpRequest method

--- client/src/main/java/com/bsds/client/http/UpicHttpClient.java
+++ client/src/main/java/com/bsds/client/http/UpicHttpClient.java
@@ -5,17 +5,26 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpRequest.Builder;
 import java.util.Arrays;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.Map.Entry;
+import java.util.function.Consumer;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 
 import java.net.http.HttpRequest;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.ValidPort;
 import org.apache.logging.log4j.LogManager;
 
+import com.bsds.client.ClientApplication;
 import com.bsds.client.model.LiftRide;
 import com.google.gson.Gson;
 
@@ -47,15 +56,40 @@ public class UpicHttpClient {
      * @throws InterruptedException
      * @throws IOException
      */
-    public static void postWriteLiftRide(String url, int time, int liftID) throws IOException, InterruptedException  {
+    // public static void postWriteLiftRide(String url, int time, int liftID) throws IOException, InterruptedException  {
 
-        // create lift ride POJO and convert to JSON string 
-        LiftRide liftRide = new LiftRide(time, liftID);
+    //     // create lift ride POJO and convert to JSON string 
+    //     LiftRide liftRide = new LiftRide(time, liftID);
+    //     String body = gson.toJson(liftRide);
+
+    //     String encodedCredentials = UpicHttpClient.getEncodedCredentials();
+    //     HttpRequest postRequest = HttpRequest.newBuilder().header("Authorization", "Basic " + encodedCredentials).uri(URI.create(url)).POST(BodyPublishers.ofString(body)).build();
+    //     HttpResponse<String> response = UpicHttpClient.getInstance().send(postRequest, HttpResponse.BodyHandlers.ofString());
+    //     int responseCode = response.statusCode();
+
+    //     logResponse(responseCode);
+    // }
+
+    public static void postLiftRide(String url, LiftRide liftRide) {
         String body = gson.toJson(liftRide);
 
         String encodedCredentials = UpicHttpClient.getEncodedCredentials();
-        HttpRequest postRequest = HttpRequest.newBuilder().header("Authorization", "Basic " + encodedCredentials).uri(URI.create(url)).POST(BodyPublishers.ofString(body)).build();
-        HttpResponse<String> response = UpicHttpClient.getInstance().send(postRequest, HttpResponse.BodyHandlers.ofString());
+        // HttpRequest postRequest = HttpRequest.newBuilder().header("Authorization", "Basic " + encodedCredentials).uri(URI.create(url)).POST(BodyPublishers.ofString(body)).build();
+
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Basic " + encodedCredentials);
+
+        HttpRequest request = buildHttpRequest(url, headers, HttpMethod.POST, body);
+        
+        HttpResponse<String> response;
+        try {
+            response = UpicHttpClient.getInstance().send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            logger.error("request failed.");
+            e.printStackTrace();
+            return;
+        }
         int responseCode = response.statusCode();
 
         logResponse(responseCode);
@@ -96,5 +130,38 @@ public class UpicHttpClient {
                 }
             }
         }
+    }
+
+    private static HttpRequest buildHttpRequest(String url, HashMap<String, String> headers, HttpMethod httpMethod, String body) {
+
+        Builder httpRequestBuilder = HttpRequest.newBuilder();
+
+        headers.forEach((k, v) -> httpRequestBuilder.header(k, v));
+
+        httpRequestBuilder.uri(URI.create(url));
+
+        switch (httpMethod) {
+            case DELETE:
+                break;
+            case GET:
+                break;
+            case HEAD:
+                break;
+            case OPTIONS:
+                break;
+            case PATCH:
+                break;
+            case POST:
+                httpRequestBuilder.POST(BodyPublishers.ofString(body));
+            case PUT:
+                break;
+            case TRACE:
+                break;
+            default:
+                break;
+            
+        }
+
+        return httpRequestBuilder.build();
     }
 }

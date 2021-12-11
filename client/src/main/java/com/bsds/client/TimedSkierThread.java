2ed7af5fee5428c92f450edcd85cf4dd9feebab7
magurman <mattgurman@gmail.com>
2021-12-11 17:17:42 -0500

    TimedSkierThread sends request to a3 post endpoint

--- client/src/main/java/com/bsds/client/TimedSkierThread.java
+++ client/src/main/java/com/bsds/client/TimedSkierThread.java
@@ -1,8 +1,16 @@
+package com.bsds.client;
+
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.bsds.client.TimedSkiersClient;
 import com.bsds.client.http.UpicHttpClient;
+import com.bsds.client.model.LiftRide;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 
 public class TimedSkierThread extends Thread {
     private final String hostname;
@@ -13,6 +21,8 @@ public class TimedSkierThread extends Thread {
     private int startSkierId;
     private int endSkierId;
     private CyclicBarrier doneBarrier;
+
+    private static final Logger logger = LogManager.getLogger(UpicHttpClient.class);
     
     public TimedSkierThread(String hostname, int port, int startSkierID, int endSkierID, 
         int startTime, int endTime, int numSkiLifts, CyclicBarrier doneBarrier){
@@ -30,27 +40,44 @@ public class TimedSkierThread extends Thread {
         while(!(TimedSkiersClient.checkDone())){
             int currentSkierID = ThreadLocalRandom.current().nextInt(this.startSkierId, this.endSkierId + 1);
             int resortID = ThreadLocalRandom.current().nextInt(0, 5);
+
             int liftID = resortID * 8 + ThreadLocalRandom.current().nextInt(0,8);
             try {
-                String url = "http://" + this.hostname + ":" + this.port
-                    +  "/skiers/" + resortID + "/seasons/2021/days/"
-                    + 1 + "/skiers/" + currentSkierID;
+                // String url = "http://" + this.hostname + ":" + this.port
+                //     +  "/skiers/" + resortID + "/seasons/2021/days/"
+                //     + 1 + "/skiers/" + currentSkierID;
+
+                String url = this.buildUrl("/liftrides");
                 // random time from range for phase
                 int randomTime = ThreadLocalRandom.current().nextInt(startTime,
                     endTime + 1);
-                // random lift 
-                // int randomLift = ThreadLocalRandom.current().nextInt(1,
-                //     numSkiLifts + 1);
+
                 long start_time = System.currentTimeMillis();
-                UpicHttpClient.postWriteLiftRide(url, randomTime, liftID); // Make POST request
-                UpicHttpClient.getVerticalForSkiDay(url);
-                long end_time = System.currentTimeMillis();
-                LatencyHistogram.update(end_time - start_time);
+                // UpicHttpClient.postWriteLiftRide(url, randomTime, liftID); // Make POST request
+
+                UpicHttpClient.postLiftRide(url, new LiftRide(randomTime, liftID, currentSkierID, resortID));
+
+                LatencyHistogram.update(System.currentTimeMillis() - start_time);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        doneBarrier.await();
+        try {
+            doneBarrier.await();
+        } catch (InterruptedException e) {
+            logger.error("Thread was interrupted.");
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            // TODO Auto-generated catch block
+            logger.error("Cyclic Barrier is broken. Could not await.");
+            e.printStackTrace();
+        }
+    }
+
+    private String buildUrl(String path) {
+
+        return "http://" + this.hostname + ":" + this.port + path;
     }
 
 }
